f7e6d3bf878f017026b04ccc2f3ed1f91cfd59d1
wogudnn <wogudnn@gmail.com>
2016-10-12 18:56:48 +0900

    ㅁㅁ

---
+++ src/main/java/net/Y5M2/user/web/ViewUserInfoModifyPageServlet.java
@@ -0,0 +1,35 @@
+package net.Y5M2.user.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.Y5M2.user.vo.UserVO;
+
+public class ViewUserInfoModifyPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ViewUserInfoModifyPageServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		String viewPath = "/WEB-INF/view/userModify.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(viewPath);
+		
+		rd.forward(request, response);
+		
+	}
+
+}
