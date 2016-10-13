d89577e7535d977061bc38ad53caf87ecd03caa6
wogudnn <wogudnn@gmail.com>
2016-10-13 18:58:59 +0900

    유저정보수정 로그인, 뷰서블릿

---
+++ src/main/java/net/Y5M2/user/web/ViewUserModifyPageServlet.java
@@ -0,0 +1,37 @@
+package net.Y5M2.user.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.Y5M2.constants.Session;
+import net.Y5M2.user.vo.UserVO;
+
+public class ViewUserModifyPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ViewUserModifyPageServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		UserVO userInfo = (UserVO) session.getAttribute(Session.USER_INFO);
+		
+		String viewPath = "/WEB-INF/view/userModify.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(viewPath);
+		request.setAttribute("userInfo", userInfo);
+		rd.forward(request, response);
+	}
+
+}
