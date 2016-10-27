4aa441a0a4f3ec5a248cc9222bd8bd053fdc86b6
kimminseg <minsegkim@naver.com>
2016-10-27 17:56:20 +0900

    ㅁㅁㅁㅁ

--- src/main/java/net/Y5M2/match/dao/MatchDaoImpl.java
+++ src/main/java/net/Y5M2/match/dao/MatchDaoImpl.java
@@ -409,64 +409,62 @@ public class MatchDaoImpl extends DaoSupport implements MatchDao {
 
 				StringBuffer query = new StringBuffer();
 
-				query.append(" SELECT	 M.MATCH_ID ");
-				query.append(" 			, M.TEAM_ID ");
-				query.append(" 			, TO_CHAR(M.SCDL, 'YYYY-MM-DD') SCDL ");
-				query.append(" 			, M.LCTN_ID ");
-				query.append(" 			, M.CRT_DT ");
-				query.append(" 			, M.TEAM_POINT ");
-				query.append(" 			, M.ATEAM_ID ");
+				query.append(" SELECT	TO_CHAR(M.SCDL,'YYYY-MM-DD') SCDL ");
 				query.append(" 			, M.PLAYFIELD ");
+				query.append(" 			, T.TEAM_ID ");
 				query.append(" 			, T.TEAM_NM ");
 				query.append(" 			, T.TEAM_PHOTO ");
-				query.append(" 			, T.LCTN_ID ");
-				query.append(" 			, T.TEAM_INFO ");
+				query.append(" 			, T2.TEAM_ID ATEAM_ID");
+				query.append(" 			, T2.TEAM_NM ATEAM_NM ");
+				query.append(" 			, T2.TEAM_PHOTO ATEAM_PHOTO ");
 				query.append(" 			, L.LCTN_NM ");
 				query.append(" 			, L.PRNT_LCTN_NM ");
 				query.append(" FROM		 MATCH M ");
 				query.append(" 			, TEAM T ");
+				query.append(" 			, TEAM T2 ");
 				query.append(" 			, LCTN L ");
 				query.append(" WHERE	M.TEAM_ID = T.TEAM_ID ");
 				query.append(" AND		M.LCTN_ID = L.LCTN_ID ");
-
+				query.append(" AND		M.ATEAM_ID = T2.TEAM_ID ");
+				query.append(" AND		M.ATEAM_ID != '0' ");
+				
 				PreparedStatement pstmt = conn.prepareStatement(query.toString());
-
 				return pstmt;
 			}
 
 			@Override
 			public Object makeObject(ResultSet rs) throws SQLException {
-
 				List<MatchVO> matchTeams = new ArrayList<MatchVO>();
 				MatchVO matchTeam = null;
 				TeamVO teamVO = null;
 				LocationVO locationVO = null;
-
+				AwayTeamVO awayTeamVO = null;
+				
 				while (rs.next()) {
 					matchTeam = new MatchVO();
-					matchTeam.setMatchId(rs.getString("MATCH_ID"));
-					matchTeam.setTeamId(rs.getString("TEAM_ID"));
 					matchTeam.setSchedule(rs.getString("SCDL"));
-					matchTeam.setLocationId(rs.getString("LCTN_ID"));
-					matchTeam.setCreateDate(rs.getString("CRT_DT"));
-					matchTeam.setMatchPoint(rs.getString("TEAM_POINT"));
-					matchTeam.setAwayTeamId(rs.getString("ATEAM_ID"));
 					matchTeam.setPlayField(rs.getString("PLAYFIELD"));
-
+	
 					teamVO = matchTeam.getTeamVO();
+					teamVO.setTeamId(rs.getString("TEAM_ID"));
 					teamVO.setTeamName(rs.getString("TEAM_NM"));
 					teamVO.setTeamPhoto(rs.getString("TEAM_PHOTO"));
-					teamVO.setLocationId(rs.getString("LCTN_ID"));
-					teamVO.setTeamInfo(rs.getString("TEAM_INFO"));
-
+					
+					awayTeamVO = matchTeam.getAwayTeamVO();
+					awayTeamVO.setTeamId(rs.getString("ATEAM_ID"));
+					awayTeamVO.setTeamName(rs.getString("ATEAM_NM"));
+					awayTeamVO.setTeamPhoto(rs.getString("ATEAM_PHOTO"));
+	
 					locationVO = matchTeam.getLocationVO();
 					locationVO.setLocationName(rs.getString("LCTN_NM"));
 					locationVO.setParentLocationName(rs.getString("PRNT_LCTN_NM"));
-
+	
 					matchTeams.add(matchTeam);
 
 				}
+
 				return matchTeams;
+
 			}
 		});
 	}
